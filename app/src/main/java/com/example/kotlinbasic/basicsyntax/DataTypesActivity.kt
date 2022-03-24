a27ef94d6a822ac2d16be089a259a5de5bd879c0
Ketan Nakum <tbs.ketan@gmail.com>
2022-03-24 11:22:16 +0530

    Add multiple operators function in OperatorsActivity

--- app/src/main/java/com/example/kotlinbasic/basicsyntax/DataTypesActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/basicsyntax/DataTypesActivity.kt
@@ -8,8 +8,89 @@ class DataTypesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_types)
+        interview()
+    }
 
+    private fun interview() {
+        //TODO("Not yet implemented")
+        dataTypes()
+        characterDataType()
+        stringDataType()
+        booleanDataTypes()
+        arrayDataTypes()
+        dataTypesConversion()
+    }
+
+    private fun dataTypesConversion() {
+        //TODO("Not yet implemented")
+        println("------------dataTypesConversion------------")
+        //Kotlin Data Type Conversion
+        /*toByte()
+        toShort()
+        toInt()
+        toLong()
+        toFloat()
+        toDouble()
+        toChar()*/
+        val x: Int = 100
+        //val y: Long = x  // Not valid assignment
+        val y: Long = x.toLong();
+
+        println(y)
+    }
+
+    private fun arrayDataTypes() {
+        //TODO("Not yet implemented")
+        //Kotlin Array Data Type
+        println("------------arrayDataTypes------------")
+        val number : IntArray = intArrayOf(1,2,3,4,5,6,7)
+        println("Value at 3rd position : " + number[2])
+    }
+
+    private fun booleanDataTypes() {
+        //TODO("Not yet implemented")
+
+        //Kotlin Boolean Data Type
+        println("------------booleanDataTypes------------")
+        val A: Boolean = true   // defining a variable with true value
+        val B: Boolean = false   // defining a variable with false value
+        val boolNull: Boolean? = null
+
+        println("Value of variable A $A")
+        println("Value of variable B $B")
+        println("Value of variable $boolNull")
+    }
+
+    private fun stringDataType() {
+        //TODO("Not yet implemented")
+        //Kotlin String Data Type
+        println("------------stringDataType------------")
+        val escapedString : String  = "I am escaped String!\n"
+        var rawString :String  = """This is going to be a 
+            multi-line string and will
+            not have any escape sequence""".trimMargin();
+
+        print(escapedString)
+        println(rawString)
+    }
+
+    private fun characterDataType() {
+        //TODO("Not yet implemented")
+        //Kotlin Character Data Type
+        println("------------characterDataType------------")
+        val letter: Char = 'A'    // defining a Char variable        // Assigning a value to it
+        println("$letter")
+
+        //  following escape sequences are supported in Kotlin: \t, \b, \n, \r, \', \", \\ and \$.
+        println('\n') //prints a newline character
+        println('\$') //prints a dollar $ character
+        println('\\') //prints a back slash \ character
+    }
+
+    private fun dataTypes() {
+        //TODO("Not yet implemented")
         //Data Types
+        println("------------dataTypes------------")
 
         val a: Int = 10000
         val d: Double = 100.00
@@ -24,51 +105,5 @@ class DataTypesActivity : AppCompatActivity() {
         println("Long Value is $l")
         println("Short Value is $s")
         println("Byte Value is $b")
-
-        //Kotlin Character Data Type
-        val letter: Char = 'A'    // defining a Char variable        // Assigning a value to it
-        println("$letter")
-
-        //  following escape sequences are supported in Kotlin: \t, \b, \n, \r, \', \", \\ and \$.
-        println('\n') //prints a newline character
-        println('\$') //prints a dollar $ character
-        println('\\') //prints a back slash \ character
-
-        //Kotlin String Data Type
-        val escapedString : String  = "I am escaped String!\n"
-        var rawString :String  = """This is going to be a 
-            multi-line string and will
-            not have any escape sequence""".trimMargin();
-
-        print(escapedString)
-        println(rawString)
-
-        //Kotlin Boolean Data Type
-        val A: Boolean = true   // defining a variable with true value
-        val B: Boolean = false   // defining a variable with false value
-        val boolNull: Boolean? = null
-
-        println("Value of variable A $A")
-        println("Value of variable B $B")
-        println("Value of variable $boolNull")
-
-        //Kotlin Array Data Type
-        val number : IntArray = intArrayOf(1,2,3,4,5,6,7)
-        println("Value at 3rd position : " + number[2])
-
-        //Kotlin Data Type Conversion
-        /*toByte()
-        toShort()
-        toInt()
-        toLong()
-        toFloat()
-        toDouble()
-        toChar()*/
-        val x: Int = 100
-        //val y: Long = x  // Not valid assignment
-        val y: Long = x.toLong();
-
-        println(y)
-
     }
 }
--- app/src/main/java/com/example/kotlinbasic/basicsyntax/OperatorsActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/basicsyntax/OperatorsActivity.kt
@@ -8,5 +8,114 @@ class OperatorsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operators)
+        interview()
+    }
+
+    private fun interview() {
+        //TODO("Not yet implemented")
+        val a: Int = 40
+        val b: Int = 20
+        arithmeticOperators(a,b)
+        relationalOperators(a,b)
+        assignmentOperators(a,b)
+        unaryOperators(a,b)
+        logicalOperators()
+        bitwiseOperators()
+    }
+    private fun arithmeticOperators(a: Int, b: Int) {
+        //Kotlin Arithmetic Operators
+        println("------------arithmeticOperators------------")
+        println("a + b = " +  (a + b))
+        println("a - b = " +  (a - b))
+        println("a / b = " +  (a / b))
+        println("a * b = " +  (a * b))
+        println("a % b = " +  (a % b))
+    }
+    private fun relationalOperators(a: Int, b: Int) {
+        //Kotlin Relational Operators
+        println("------------relationalOperators------------")
+        println("a > b = " +  (a > b))
+        println("a < b = " +  (a < b))
+        println("a >= b = " +  (a >= b))
+        println("a <= b = " +  (a <= b))
+        println("a == b = " +  (a == b))
+        println("a != b = " +  (a != b))
+    }
+    private fun assignmentOperators(a: Int, b: Int) {
+        //Kotlin Assignment Operators
+        println("------------assignmentOperators------------")
+        println("a = $a")
+        println("b = $b")
+        var a : Int =  a
+
+        a += 10
+        println("Resign value of a = $a")
+
+        a += 5
+        println("a += 5 = $a")
+
+        a = 40;
+        a -= 5
+        println("a -= 5 = $a")
+
+        a = 40
+        a *= 5
+        println("a *= 5 = $a")
+
+        a = 40
+        a /= 5
+        println("a /= 5 = $a")
+
+        a = 43
+        a %= 5
+        println("a %= 5 = $a")
+    }
+    private fun unaryOperators(a: Int, b: Int){
+        //Kotlin Unary Operators
+        println("------------Unary Operators------------")
+        var x: Int = a
+        var b:Boolean = true
+
+        println("+x = " +  (+x))
+        println("-x = " +  (-x))
+        println("++x = " +  (++x))
+        println("--x = " +  (--x))
+        println("!b = " +  (!b))
+    }
+    private fun logicalOperators() {
+        //Kotlin Logical  Operators
+        println("------------Logical Operators------------")
+
+        var x: Boolean = true
+        var y:Boolean = false
+
+        println("x && y = " +  (x && y))
+        println("x || y = " +  (x || y))
+        println("!y = " +  (!y))
+    }
+    private fun bitwiseOperators() {
+        //Kotlin Bitwise Operators
+        println("------------bitwiseOperators------------")
+        var x:Int = 60	  // 60 = 0011 1100
+        var y:Int = 13	  // 13 = 0000 1101
+        var z:Int
+
+        z = x.shl(2)       // 240 = 1111 0000
+        println("x.shl(2) = $z")
+
+        z = x.shr(2)       // 15 = 0000 1111
+        println("x.shr(2) = $z")
+
+        z = x.and(y)       // 12 = 0000 1100
+        println("x.and(y)  = $z")
+
+        z = x.or(y)        // 61 = 0011 1101
+        println("x.or(y)  = $z")
+
+        z = x.xor(y)       // 49 = 0011 0001
+        println("x.xor(y)  = $z")
+
+        z = x.inv()        // -61 = 1100 0011
+        println("x.inv()  = $z")
     }
 }
